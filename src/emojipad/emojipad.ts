448af84dafdd842d970a2509f2555e5f3e23559a
Aleksei Korzun <akorzun@outlook.com>
2020-12-17 00:35:38 +0300

    tweak emoji size

--- src/emojipad/emojipad.ts
+++ src/emojipad/emojipad.ts
@@ -20,13 +20,13 @@ class NullContextError extends Error {
 export class EmojiPad {
   static emojis = ['1f0cf', '1f30d', '1f31d', '1f336-fe0f', '1f346', '1f34a', '1f34b', '1f34c', '1f351', '1f355', '1f369', '1f36a', '1f37f', '1f389', '1f3b2', '1f3b7', '1f3b8', '1f3c2', '1f3c6', '1f419', '1f42d', '1f437', '1f438', '1f44c', '1f47b', '1f47d', '1f47e', '1f480', '1f48e', '1f4a3', '1f4a9', '1f4ce', '1f4e3', '1f52a', '1f52b', '1f600', '1f602', '1f606', '1f607', '1f609', '1f60d', '1f610', '1f618', '1f61b', '1f621', '1f626', '1f628', '1f62c', '1f62d', '1f62e', '1f631', '1f633', '1f634', '1f636', '1f637', '1f644', '1f680', '1f6b2', '1f6f8', '1f6f9', '1f6fc', '1f911', '1f912', '1f913', '1f914', '1f916', '1f91f', '1f921', '1f923', '1f924', '1f929', '1f92a', '1f92b', '1f92c', '1f92e', '1f92f', '1f951', '1f955', '1f95e', '1f965', '1f96b', '1f970', '1f972', '1f973', '1f975', '1f976', '1f980', '1f981', '1f984', '1f98a', '1f98b', '1f9c0', '1f9c1', '1f9da', '1f9dc-200d-2640-fe0f', '1f9e0', '1fa82', '1fa99', '1fab2', '1fad2', '263a-fe0f', '2693', '270c-fe0f'];
 
-  layersAmount: number;
+  layersAmount: number = 100;
   layers: Layer[] = [];
   usedIndexes: number[] = [];
 
   width: number;
   height: number;
-  ratio: number;
+  ratio: number = window.devicePixelRatio || 1;
   emojiSize: number;
 
   ctx: CanvasRenderingContext2D;
@@ -34,14 +34,12 @@ export class EmojiPad {
   private resizeTimer?: number;
 
   constructor(public container: HTMLElement) {
-    this.ratio = window.devicePixelRatio || 1;
     const mobileDevice = window.innerWidth < 768;
 
     this.width = container.clientWidth * this.ratio;
     this.height = container.clientHeight * this.ratio;
 
-    this.layersAmount = mobileDevice ? 49 : 99;
-    this.emojiSize = Math.max(window.innerHeight, window.innerWidth) / (mobileDevice ? 5 : 10);
+    this.emojiSize = Math.min(window.innerHeight, window.innerWidth) / 7 * this.ratio;
 
     const canvas = document.createElement('canvas');
 
@@ -132,7 +130,7 @@ export class EmojiPad {
   }
 
   fillLayers() {
-    for (let i = this.layersAmount; i > 0; i--) {
+    for (let i = this.layersAmount; i >= 0; i--) {
       this.fillLayer();
     }
   }
